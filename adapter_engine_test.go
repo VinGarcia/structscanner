2032af4f1a3e41a37ee92e576a6f7f47363825dc
Vinícius Garcia <vingarcia00@gmail.com>
2022-06-08 22:34:36 -0300

    Remove direct references to testify
    We are now using the testtools helper for everything

--- adapter_engine_test.go
+++ adapter_engine_test.go
@@ -3,7 +3,6 @@ package tagmapper_test
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/vingarcia/tagmapper"
 	tt "github.com/vingarcia/tagmapper/helpers/testtools"
 )
@@ -24,8 +23,8 @@ func TestUnmarshal(t *testing.T) {
 			Attr1 string `env:"attr1"`
 		}
 		err := tagmapper.Decode(decoder, &output)
-		assert.Nil(t, err)
-		assert.Equal(t, output.Attr1, "fake-value-for-string")
+		tt.AssertNoErr(t, err)
+		tt.AssertEqual(t, output.Attr1, "fake-value-for-string")
 	})
 
 	t.Run("should ignore attributes if the function returns a nil value", func(t *testing.T) {
@@ -44,9 +43,9 @@ func TestUnmarshal(t *testing.T) {
 		}
 		output.Attr2 = "placeholder"
 		err := tagmapper.Decode(decoder, &output)
-		assert.Nil(t, err)
-		assert.Equal(t, output.Attr1, "fake-value-for-string")
-		assert.Equal(t, output.Attr2, "placeholder")
+		tt.AssertNoErr(t, err)
+		tt.AssertEqual(t, output.Attr1, "fake-value-for-string")
+		tt.AssertEqual(t, output.Attr2, "placeholder")
 	})
 
 	t.Run("should report errors correctly", func(t *testing.T) {
